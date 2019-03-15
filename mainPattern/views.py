b528231ead08086ce21c8e4e551c49f859c35108
TrimokRu <prokirik@gmail.com>
2019-03-15 19:09:33 +0300

    home page

--- mainPattern/views.py
+++ mainPattern/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'mainPattern/home.html')
+    years = range(1902, 2006);
+    months = range(1, 13);
+    days = range(1, 32,)
+    return render(request, 'mainPattern/home.html', {'years': years, 'months' : months, 'days': days,})
 
 def login(request):
     return render(request, 'mainPattern/login.html')
